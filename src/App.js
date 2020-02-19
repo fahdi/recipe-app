6d40288d1bcc90356b537f400924d7485dc6e53a
Fahad Murtaza <fahdim@gmail.com>
2020-02-19 11:14:31 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const APP_ID = "5bc34c63";
+  const APP_KEY = "62c931b989b3e81b6c36c72bf1ece52f"
+  const API_PATH = "https://api.edamam.com/search"; 
+  const SEARCH_PATH = "${API_PATH}?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}";
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+      console.log("This has been run");
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+      <input className="search-bar" type="text" />
+      <button className="search-button" type="submit" />
+      <h1 onClick={()=>setCounter(counter+1)}>{counter}</h1>
+      </form>
     </div>
   );
 }
